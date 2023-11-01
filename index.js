2f5aeccb9288ffea5b73577fac459ec476528476
Karuchiu <karuchiu8@gmail.com>
2023-11-01 18:53:32 +0300

    Too easy

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function saturdayFun(task = "roller-skate"){
+    return `This Saturday, I want to ${task}!`
+}
+
+const mondayWork = function(task = 'go to the office'){
+    return `This Monday, I will ${task}.`
+}
+
+function wrapAdjective(params = '*') {
+    return function(param = 'special'){
+        return `You are ${params}${param}${params}!`
+    }
+}
